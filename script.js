08944ab798ea82134360cc4a7baa98ec0eb14c83
Kishor U <blackkspydo@gmail.com>
2024-07-10 11:58:01 +0545

    added confetti on done

--- script.js
+++ script.js
@@ -40,9 +40,51 @@ function deleteTodo(id) {
     render();
 }
 
-function toggleTodo(id) {
+function completeTodo(id) {
     const todo = state.todos.find(todo => todo.id === id);
     if (todo) todo.done = !todo.done;
+    if (todo.done) {
+        const count = 200,
+            defaults = {
+                origin: { y: 0.7 },
+            };
+
+        function fire(particleRatio, opts) {
+            confetti(
+                Object.assign({}, defaults, opts, {
+                    particleCount: Math.floor(count * particleRatio),
+                })
+            );
+        }
+
+        fire(0.25, {
+            spread: 26,
+            startVelocity: 55,
+        });
+
+        fire(0.2, {
+            spread: 60,
+        });
+
+        fire(0.35, {
+            spread: 100,
+            decay: 0.91,
+            scalar: 0.8,
+        });
+
+        fire(0.1, {
+            spread: 120,
+            startVelocity: 25,
+            decay: 0.92,
+            scalar: 1.2,
+        });
+
+        fire(0.1, {
+            spread: 120,
+            startVelocity: 45,
+        });
+    }
+    deleteTodo(id);
     saveToLocalStorage();
     render();
 }
@@ -137,7 +179,7 @@ function addEventListeners() {
     });
 
     document.querySelectorAll('.done-button').forEach(button => {
-        button.addEventListener('click', (e) => toggleTodo(parseInt(e.target.dataset.id)));
+        button.addEventListener('click', (e) => completeTodo(parseInt(e.target.dataset.id)));
     });
 
     document.querySelectorAll('.edit-button').forEach(button => {
